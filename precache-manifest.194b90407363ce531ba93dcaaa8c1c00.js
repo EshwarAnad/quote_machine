f4ab640e8890083dddbb98851d3443f877963a2b
Carrie <chanscom7@gmail.com>
2020-09-12 15:14:03 -0400

    Updates

---
+++ precache-manifest.194b90407363ce531ba93dcaaa8c1c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74b870c411fd8c5a2a87c344a221c8c",
+    "url": "/quote_machine/index.html"
+  },
+  {
+    "revision": "f07f41706d332e2fa787",
+    "url": "/quote_machine/static/css/main.4a4f9111.chunk.css"
+  },
+  {
+    "revision": "68f2ffa62dfa57f50a6f",
+    "url": "/quote_machine/static/js/2.c0c699a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote_machine/static/js/2.c0c699a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07f41706d332e2fa787",
+    "url": "/quote_machine/static/js/main.a4c3d803.chunk.js"
+  },
+  {
+    "revision": "2f79519e65c64cec404e",
+    "url": "/quote_machine/static/js/runtime-main.b0c1dc7a.js"
+  }
+]);
